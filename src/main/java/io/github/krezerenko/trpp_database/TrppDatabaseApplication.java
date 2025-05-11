44f29952ca4de2961618087c1c8d58cf72553a08
Krezon <krezony@gmail.com>
2025-05-11 20:16:29 +0300

    Создал Spring проект
    Для создания использовался Spring Initializr
    Проект предполагается быть сервером

---
+++ src/main/java/io/github/krezerenko/trpp_database/TrppDatabaseApplication.java
@@ -0,0 +1,13 @@
+package io.github.krezerenko.trpp_database;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrppDatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrppDatabaseApplication.class, args);
+	}
+
+}
